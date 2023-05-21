bd157508cc87ef310db917078228158fceb01287
OctaCore <86001641+OctaCoreWasTaken@users.noreply.github.com>
2023-05-21 22:23:38 +0300

    Delete Car directory

--- Car/Unused code.cs
+++
@@ -1,7 +0,0 @@
-        // float MzLookup = Mz.Evaluate(Mathf.Abs(SA));
-        // if (SA < 0){
-        //     MzLookup *= -1;
-        // }
-        // if (Mathf.Abs(steer) != 1 && Mathf.Round(Mathf.Abs(SA)) > 0 && FrontTire){
-        //     //Epic code for ffb :thumbsup:
-        // }
